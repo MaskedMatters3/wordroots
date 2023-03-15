ef45e028e26d148de4eac5d048a9ff8c27248788
MaskedMatters3 <120686253+MaskedMatters3@users.noreply.github.com>
2023-03-14 20:24:54 -0400

    Adding additional COOLNESS

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// function afteranim() {
-//     setTimeout(anim2(), 3000);
-// }
+var gameStart;
 
-// function anim2() {
-//     var element = document.getElementById('anim1');
-//     element.classList.add("afteranimate");
-// }
+function afteranim() {
+    console.log("TEST");
+
+    setTimeout(() => {
+        console.log("TEST2");
+        var element = document.getElementById('cred');
+        element.classList.add('anim2');
+        element.style.top = "-200px"
+        anim2();
+    }, 1500);
+}
+
+function anim2() {
+    setTimeout(() => {
+        console.log("TEST3");
+        var element = document.getElementById('theGame');
+        element.style.top = "-100px"
+        element.classList.add('anim3')
+    }, 1000)
+}
 
 document.onkeyup = function(e) {
     if(e.keyCode == 32) {
